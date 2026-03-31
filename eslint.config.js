d9c874b9b312fa94c0c8cfb253e5a7b578fce39a
Sean Fang <silence.boy@live.cn>
2026-04-01 00:55:28 +0800

    feat: 添加 eslint-config-ali 依赖

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
+import { react as aliReact } from 'eslint-config-ali'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist','coverage']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+  ...aliReact,
+  reactRefresh.configs.vite,
   {
     files: ['src/**/*.{ts,tsx}'],
     ignores: [
